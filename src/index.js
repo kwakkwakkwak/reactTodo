714fc166711cc47d6c5faf47e5f3d468d7c39311
kwakkwakkwak <kwakkj112@naver.com>
2019-04-28 18:07:33 +0900

    react todo project start

--- src/index.js
+++ src/index.js
@@ -1,12 +1,105 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function AddedTask(props) {
+        return(
+        <div className="AddedTask">
+            <label onClick={props.toggleStatus}>
+                {props.status}
+            </label>
+            <label >{props.task}</label>
+            <label onClick={props.removeTask}>
+                -
+            </label>
+        </div>
+        );
+}
+class Task extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            task : "Write your Task",
+            status : "○", // null , added , done
+        };
+
+
+    }
+    render() {
+        return (
+            <div className="task">
+                <input placeholder={this.state.task}/>
+
+                <label onClick={this.addTask}>
+                    +
+                </label>
+            </div>
+        );
+    }
+
+    addTask(){
+        alert("add Task!");
+
+    }
+
+
+}
+
+
+class Todo extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state= {
+            task: "aaaaaaaaaaa",
+            status: "○",
+        };
+
+        // this.toggleStatus = this.toggleStatus.bind(this);
+
+
+
+    }
+
+
+    toggleStatus(){
+        this.setState({
+            status : this.state.status === "○" ? "●" : "○",
+        });
+    }
+
+    removeTask(){
+        alert("remove Task!");
+
+    }
+
+
+
+
+
+    render() {
+        return(
+        <div>
+
+            react-todo Start !!
+            <AddedTask
+                removeTask={() => this.removeTask()}
+                toggleStatus = {() => this.toggleStatus()}
+                task = {this.state.task}
+                status = {this.state.status}
+                />
+
+            <Task></Task>
+
+        </div>
+        );
+    }
+}
+
+
+
+ReactDOM.render(
+    <Todo />,
+    document.getElementById('root')
+);
